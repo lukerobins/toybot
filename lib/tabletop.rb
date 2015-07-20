b8dbbde43a159c9cff45a5cacafccb837cf1488c
Luke Robins <luker667@gmail.com>
2015-07-21 00:47:39 +1000

    validate x,y postion on tabletop

--- lib/tabletop.rb
+++ lib/tabletop.rb
@@ -5,4 +5,21 @@ class Tabletop
     @height = height
     @width = width
   end
+
+  def valid_position? x, y
+    return false if invalid_x? x
+    return false if invalid_y? y
+    true
+  end
+
+  private
+
+  def invalid_x? x
+    x < 0 or x > @width
+  end
+
+  def invalid_y? y
+    y < 0 or y > @height
+  end
+
 end
--- spec/tabletop_spec.rb
+++ spec/tabletop_spec.rb
@@ -6,14 +6,28 @@ describe Tabletop do
   let!(:tabletop) { Tabletop.new( 7,7 ) }
 
   describe '#height' do
-    it 'return tabletop height' do
+    it 'returns tabletop height' do
       expect(tabletop.height).to eq 7
     end
   end
 
   describe '#width' do
-    it 'return tabletop width' do
+    it 'returns tabletop width' do
       expect(tabletop.width).to eq 7
+    end
+  end
+
+  describe '#valid_position?' do
+    it 'returns true if x and y within tabletop' do
+      expect(tabletop.valid_position? 2,6).to be true
+    end
+
+    it 'returns false if x outside tabletop' do
+      expect(tabletop.valid_position? 8,6).to be false
+    end
+
+    it 'returns false if y outside tabletop' do
+      expect(tabletop.valid_position? 1,9).to be false
     end
   end
 end
