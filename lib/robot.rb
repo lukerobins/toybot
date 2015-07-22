a3be31310e501b8b4650aa78e76e90e4019869a3
Luke Robins <luker667@gmail.com>
2015-07-23 01:39:08 +1000

    Add turn and move to Robot

--- lib/robot.rb
+++ lib/robot.rb
@@ -20,6 +20,23 @@ class Robot
     end
   end
 
+  def move
+    return false unless @tabletop
+    new_position = Movement.move @position, 1
+    if @tabletop.valid_position? new_position.x, new_position.y
+      @position = new_position
+      true
+    else
+      false
+    end
+  end
+
+  def turn direction
+    return false unless @tabletop
+    @position.facing = Movement.turn @position.facing, direction.downcase.to_sym
+    true
+  end
+
   private
 
   def valid_placement? position, tabletop
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -76,4 +76,73 @@ describe Robot do
     end
 
   end
+
+  describe '#move' do
+    let(:robot) do
+      tabletop = Tabletop.new(5,5)
+      robot = Robot.new
+      robot.place(Position.new(0,0,:North),tabletop)
+      robot
+    end
+
+    context 'valid move' do
+      it 'move one :North' do
+        robot.move
+        expect(robot.position.y).to eq 1
+      end
+
+      it 'returns true' do
+        expect(robot.move).to be true
+      end
+    end
+
+    context 'invalid move' do
+      let(:position) {Position.new(0,0,:South)}
+
+      before do
+        robot.place(position)
+      end
+
+      it 'does not move' do
+        robot.move
+        expect(robot.position).to eq position
+      end
+
+      it 'returns false' do
+        expect(robot.move).to be false
+      end
+    end
+  end
+
+  describe '#turn' do
+    context 'unplaced robot' do
+      let(:robot) { Robot.new }
+      it 'returns false' do
+        expect(robot.turn :left).to be false
+      end
+    end
+
+    context 'placed robot' do
+      let(:robot) do
+        tabletop = Tabletop.new(5,5)
+        robot = Robot.new
+        robot.place(Position.new(0,0,:North),tabletop)
+        robot
+      end
+
+      it 'returns true' do
+        expect(robot.turn :left).to be true
+      end
+
+      it 'turns to left to :West' do
+        robot.turn :left
+        expect(robot.position.facing).to eq :West
+      end
+
+      it 'turns to right to :East' do
+        robot.turn 'RIGHT'
+        expect(robot.position.facing).to eq :East
+      end
+    end
+  end
 end
