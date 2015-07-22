210c12452a1538047667a2aa685d2631a674142c
Luke Robins <luker667@gmail.com>
2015-07-23 00:58:15 +1000

    Place method for robot

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,10 +1,28 @@
 class Robot
-  require 'position'
+  require 'movement'
 
   attr_reader :position, :tabletop
 
   def initialize
     @position = nil
     @tabletop = nil
+  end
+
+  def place position, tabletop=nil
+    tabletop = tabletop || @tabletop #can place on new tabletop
+    raise 'robot has no tabletop' unless tabletop
+    if valid_placement? position, tabletop
+      @position = position
+      @tabletop = tabletop
+      true
+    else
+      false
+    end
+  end
+
+  private
+
+  def valid_placement? position, tabletop
+    tabletop.valid_position?(position.x, position.y) && Movement.valid_direction?(position.facing)
   end
 end
